b06d53b3a59ae16fa2f769f7cca605a67f8d245a
Daniel Stocker <stdrs02@moravian.edu>
2019-02-15 15:07:59 -0500

    Added redis endpoints

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, Response
+import redis
 
+r = redis.Redis()
 import json
 app = Flask(__name__)
 
@@ -14,6 +16,21 @@ def default():
 @app.route('/testEndpoint')
 def testEndpoint():
     return "You did it"
+
+@app.route('/addToRedis')
+def addToRedis():
+    r.set('key', 'value')
+    return 'Added'
+
+@app.route('/testGetRedis')
+def testGetRedis():
+    return_value = r.get('key')
+    return return_value
+
+@app.route('deleteRedisData')
+def deleteRedisData():
+    r.delete('key')
+    return 'Deleted'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
